5b8959e79f9911bb1567e3e5a5b4dad2525c5077
Ana WU <ana_wu@ceroca.cat>
2024-09-23 19:55:55 +0200

    Moviment Jugador

---
+++ Assets/Scripts/NauJugador.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private float vel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vel = 8f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direccioIndicadaX = Input.GetAxisRaw("Horizontal");
+        float direccioIndicadaY = Input.GetAxisRaw("Vertical");
+        //Debug.Log("X :" + direccioIndicadaX + "-Y:" + direccioIndicadaY);
+        Vector2 direccioIndicada = new Vector2(direccioIndicadaX,direccioIndicadaY);
+
+
+        Vector2 novaPos = transform.position;// transform.position:pos actual de la nau.
+        novaPos = novaPos + direccioIndicada * vel * Time.deltaTime;
+        //Debug.Log(Time.deltaTime);
+        transform.position = novaPos;
+    }
+}
